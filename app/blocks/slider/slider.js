652f3a2e72dd700a14a66c3bf7cb36b74e147005
r.kirilenko <r.kirilenko@livestream.ltd>
2020-03-24 00:37:52 +0300

    validation page and faq page. some works on main page

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -2,7 +2,7 @@ app.slider = {
 	init(selector, options) {
 		app.common.initScript('swiper.min', 'mySwiper', () => {
 			this.runSlider('.swiper-rules', options || this.options);
-			this.runSlider('.rating__slider .swiper-container', this.options);
+			this.runSlider('.rating__slider .swiper-container', this.optionsRating);
 			this.runSlider('.swiper-gifts', this.options);
 		});
 		app.common.initStyle('swiper.min');
@@ -11,6 +11,15 @@ app.slider = {
 		const slider = new Swiper(selector, options);
 	},
 	options: {
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+		slidesPerView: 1.15,
+		centeredSlides: true,
+		spaceBetween: 10
+	},
+	optionsRating: {
 		pagination: {
 			el: '.swiper-pagination',
 			clickable: true
--- app/scripts/init.js
+++ app/scripts/init.js
@@ -2,3 +2,5 @@ app.common.init();
 app.sliderServices.init();
 app.fancyboxExample.init();
 app.slider.init();
+app.enter.init();
+app.questions.init();
